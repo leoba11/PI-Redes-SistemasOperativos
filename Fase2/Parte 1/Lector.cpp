d4d0762b4fcef69254ba353d3e5060d9a6123aa9
royerchq <48787210+royerchq@users.noreply.github.com>
2019-04-02 21:52:04 -0600

    Add files via upload

--- Fase2/Parte 1/Lector.cpp
+++ Fase2/Parte 1/Lector.cpp
@@ -7,80 +7,112 @@
 
 using namespace std;
 
-void lector(const char* ruta);
+void lector();
 int main(int argc, char **argv){
-   lector("/home/royerchq/Downloads/prueba");
+   lector();
 }
 
 
 
-void lector(const char* ruta){
+void lector(){
+	char* rut;
+	const char* ruta;
 	int contador=0;
 	int tam=0;
     int f=0;
     int cont_imagen=0;
     bool esImagen=false;
     string ruta_cont;
-    string exten="gnp.";
+    string exten;
 	vector<string> rutas;
 	string fileName;
-	DIR* pDIR ;
+	DIR* pDIR;
 	string ruta_aux;
+	int formato=0;
+	int seguir=1;
+   
+
+   
+        cout<<"Digite 1 para una nueva ruta o 0 para terminar "<<endl;
+        cin >> seguir;
 	
-	if(pDIR = opendir(ruta)){
-        while(dirent* entry = readdir(pDIR)) {
-            fileName = entry->d_name;
+    
+	while(seguir){
 
-         
-        tam = fileName.size()-4;
-        f=0;
-        cont_imagen=0;
-        esImagen=false;
-        for(int i= fileName.size()-1; i >= tam ; i--){
-        	
-        	if(fileName[i]==exten[f]){
-        	
-        		esImagen=true;
-        		cont_imagen++;
-        	
-        	}else{
-        		
-        		esImagen=false;
+		 cout<<"Digite 1 para formtato png \n 2 para formato gif \n 3 para formatio jpg"<<endl;
+         cin >> formato;
+		 switch(formato) {
+            case 1 : exten="gnp.";
+               break;       
+            case 2 : exten="fig.";
+               break;
+            case 3 : exten="gpj.";
+               break;
+    }
+			cout<<"Digite la ruta"<<endl;
+        cin >> rut;
+        ruta=rut;
 
-        	}
-        	
-        	f++;
-        }
-       
-        if(esImagen && cont_imagen==4){
-        	ruta_aux=ruta;
-        	rutas.push_back(ruta_aux+"/"+fileName);
-        	cout << fileName << '\n';
-        }
-   }
 
-    closedir(pDIR);
-     
-     while(rutas.size() != 0 ){
-     	
-     	if(contador < 2){
-     		contador++;
-     		//ruta_cont = rutas.back();
-     		//rutas.pop_back();
-     	
-     		if(fork() == 0){
-     			//scontador++;
-     			cout<<"hijo"<<contador<<endl;
-    			//contratista(ruta_cont, &contador);
-     		    ruta_cont = rutas.back();
-     		    rutas.pop_back();
-     	
 
-     		}	
-     	}
+	        if(pDIR = opendir(ruta)){
+	        while(dirent* entry = readdir(pDIR)) {
+	            fileName = entry->d_name;
 
-     }
- }
+	         
+	         tam = fileName.size()-4;
+	         f=0;
+	         cont_imagen=0;
+	         esImagen=false;
+	         for(int i= fileName.size()-1; i >= tam ; i--){
+	        	
+	        	if(fileName[i]==exten[f]){
+	        	
+	        		esImagen=true;
+	        		cont_imagen++;
+	        	
+	        	}else{
+	        		
+	        		esImagen=false;
+
+	        	}
+	        	
+	        	f++;
+	        }
+	       
+	        if(esImagen && cont_imagen==4){
+	        	ruta_aux=ruta;
+	        	rutas.push_back(ruta_aux+"/"+fileName);
+	        	cout << fileName << '\n';
+	        }
+	    }
+
+	     closedir(pDIR);
+	     
+	     /*while(rutas.size() != 0 ){
+	     	
+	     	if(contador < 2){
+	     		contador++;
+	     		cont_imagen+=2;
+	     		ruta_cont = rutas.back();
+	     		rutas.pop_back();
+	     	
+	     		if(fork() == 0){
+	    			//contratista(ruta_cont, contador_imagen);
+	     		}
+	     	}
+	    }*/
+    }
+
+    cout<<"Digite 1 para una nueva ruta o 0 para terminar "<<endl;
+        cin >> seguir;
+		
+
+
+}
+
+
+	
 
 }
 
