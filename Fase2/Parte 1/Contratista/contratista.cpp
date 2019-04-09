c5b09ad57b216e84132ccc8b2446877e372562b5
josuearaya <42825942+josuearaya@users.noreply.github.com>
2019-04-09 04:33:15 -0600

    Hilos al contratista

--- Fase2/Parte 1/Contratista/contratista.cpp
+++ Fase2/Parte 1/Contratista/contratista.cpp
@@ -6,6 +6,8 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -15,9 +17,162 @@ struct msgbuffer
     unsigned char mtext[128];
 };
 
-void contratista(string nombre, string direccion)
+struct big_buffer
+{
+    int bandera = 0;
+    unsigned char bytes[512];
+};
+
+struct small_buffer
+{
+    int bandera = 0;
+    unsigned char bytes[512];
+};
+
+void leer_bytes(vector<big_buffer> &vector512, string direccion)
 {
     FILE *imagen;
+    int index = 0;
+    struct big_buffer bigBuffer;
+    unsigned char buffer[512];
+
+    imagen = fopen(direccion.c_str(), "rb");
+
+    if (imagen == NULL)
+    {
+        cout << "Error al leer la imagen" << endl;
+        exit(1);
+    }
+
+    while (fread(buffer, 512, 1, imagen) == 1)
+    {
+        while (true)
+        {
+            if (vector512.at(index).bandera == 0)
+            {
+                memcpy(bigBuffer.bytes, buffer, sizeof(buffer));
+                bigBuffer.bandera = 1;
+                vector512.at(index) = bigBuffer;
+
+                index = (index + 1) % 2;
+                
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    while (true)
+    {
+        if (vector512.at(index).bandera == 0)
+        {
+            memcpy(bigBuffer.bytes, "Fin", sizeof("Fin"));
+            bigBuffer.bandera = 2;
+            vector512.at(index) = bigBuffer;
+            
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    fclose(imagen);
+}
+
+void dividir_bytes(vector<big_buffer> &vector512, vector<small_buffer> &vector128)
+{
+    int bigIndex = 0;
+    int smallIndex = 0;
+    struct small_buffer smallBuffer;
+
+    while (vector512.at(bigIndex).bandera != 2)
+    {
+        if (vector512.at(bigIndex).bandera == 1)
+        {
+            for (int i = 0; i < 512; i += 128)
+            {
+                while (true)
+                {
+                    if (vector128.at(smallIndex).bandera == 0)
+                    {
+                        memcpy(smallBuffer.bytes, vector512.at(bigIndex).bytes + i, 128);
+                        smallBuffer.bandera = 1;
+                        vector128.at(smallIndex) = smallBuffer;
+
+                        smallIndex = (smallIndex + 1) % 6;
+                        
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            vector512.at(bigIndex).bandera = 0;
+
+            bigIndex = (bigIndex + 1) % 2;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    while (true)
+    {
+        if (vector128.at(smallIndex).bandera == 0)
+        {
+            memcpy(smallBuffer.bytes, "Fin", sizeof("Fin"));
+            smallBuffer.bandera = 2;
+            vector128.at(smallIndex) = smallBuffer;
+
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
+
+void enviar_bytes(vector<small_buffer> &vector128, long mtype, int cola_paquetes)
+{
+    int smallIndex = 0;
+    struct msgbuffer mensaje;
+
+    while (vector128.at(smallIndex).bandera != 2)
+    {
+        while (true)
+        {
+            if (vector128.at(smallIndex).bandera == 1)
+            {
+                memcpy(mensaje.mtext, vector128.at(smallIndex).bytes, 128);
+                mensaje.mtype = mtype;
+                msgsnd(cola_paquetes, &mensaje, sizeof(mensaje.mtext), 0);
+
+                vector128.at(smallIndex).bandera = 0;
+                
+                smallIndex = (smallIndex + 1) % 6;
+                
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
+
+void contratista(string nombre, string direccion)
+{
     int cola_mtype;
     int cola_paquetes;
     int memoria_compartida;
@@ -28,9 +183,9 @@ void contratista(string nombre, string direccion)
     key_t llave_semaforo = 0x1111;
     long mtype;
     struct msgbuffer mensaje;
-    unsigned char buffer[512];
     unsigned char fin[17];
-    unsigned char paquete[128];
+    vector<big_buffer> vector512;
+    vector<small_buffer> vector128;
     void *contador;
 
     cola_mtype = msgget(llave_cola_mtype, 0666);
@@ -65,15 +220,6 @@ void contratista(string nombre, string direccion)
         exit(1);
     }
 
-    direccion.append(nombre);
-    imagen = fopen(direccion.c_str(), "rb");
-
-    if (imagen == NULL)
-    {
-        cout << "Error al crear la imagen" << endl;
-        exit(1);
-    }
-
     while (true)
     {
         if (msgrcv(cola_mtype, &mensaje, sizeof(mensaje.mtext), 1, 0) > 0)
@@ -87,16 +233,17 @@ void contratista(string nombre, string direccion)
     mensaje.mtype = mtype;
     msgsnd(cola_paquetes, &mensaje, sizeof(mensaje.mtext), 0);
 
-    while (fread(buffer, 512, 1, imagen) == 1)
-    {
-        for (int i = 0; i < 512; i += 128)
-        {
-            memcpy(paquete, buffer + i, 128);
-            memcpy(mensaje.mtext, paquete, sizeof(paquete));
-            mensaje.mtype = mtype;
-            msgsnd(cola_paquetes, &mensaje, sizeof(mensaje.mtext), 0);
-        }
-    }
+    vector512.resize(2);
+    vector128.resize(6);
+    direccion.append(nombre);
+
+    thread t1(leer_bytes, ref(vector512), direccion);
+    thread t2(dividir_bytes, ref(vector512), ref(vector128));
+    thread t3(enviar_bytes, ref(vector128), mtype, cola_paquetes);
+
+    t1.join();
+    t2.join();
+    t3.join();
 
     strcpy((char *)mensaje.mtext, "Fin de la imagen");
     mensaje.mtype = mtype;
@@ -135,6 +282,4 @@ void contratista(string nombre, string direccion)
     }
 
     shmdt(contador);
-
-    fclose(imagen);
 }
